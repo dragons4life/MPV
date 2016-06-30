b1936e635548031cb4934b505ab30ef3d374b9de
dragons4life <motionzmedia@gmail.com>
2016-06-30 00:26:17 -0400

    Add files via upload

--- mpv/updater.ps1
+++ mpv/updater.ps1
@@ -1,11 +1,11 @@
-$url = "https://bitbucket.org/rorgoroth/mpv-for-windows/downloads/mpv-latest-Win64.zip"
-$output = "mpv-latest-Win64.zip"
+$url = "https://bitbucket.org/rorgoroth/mpv-for-windows/downloads/mpv-update-Win64.zip"
+$output = "mpv-update-Win64.zip"
 
 Invoke-WebRequest -Uri $url -OutFile $output
 $shell_app= new-object -com shell.application
-$filename = "mpv-latest-Win64.zip"
+$filename = "mpv-update-Win64.zip"
 $zip_file = $shell_app.namespace((Get-Location).Path + "\$filename")
 $destination = $shell_app.namespace((Get-Location).Path)
 $destination.Copyhere($zip_file.items())
 
-Remove-Item -Force .\mpv-latest-Win64.zip
+Remove-Item -Force .\mpv-update-Win64.zip
